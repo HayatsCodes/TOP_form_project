dc6759f8c2faa81167a791ed4cf25f6c05a5a254
Hayats <hayatudeenabdulrahman@gmail.com>
2023-08-11 15:38:00 +0100

    improved the validatePassword function for effective validation

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const pswInput = document.getElementById('password');
 const confirmPswInput = document.getElementById('confirm-password');
 
 function validatePassword() {
-    console.log(pswInput.value, confirmPswInput.value)
-    if (pswInput.value !== confirmPswInput.value) {
+    console.log(pswInput.value < 8)
+    if (pswInput.value < 8 || pswInput.value !== confirmPswInput.value) {
         confirmPswInput.style.border = 'none';
         confirmPswInput.style.outline = '1px solid red';
     } else {
